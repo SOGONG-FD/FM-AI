5cb0b797034578d8b2211027a2f88f12309ab772
Park jaemin <hungryshark4292@gmail.com>
2025-06-04 15:09:44 +0900

    db

--- database.py
+++ database.py
@@ -7,16 +7,8 @@ import os
 # .env 파일 로드
 load_dotenv()
 
-# 환경변수에서 데이터베이스 정보 가져오기
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
-
-# 데이터베이스 URL 생성
-DB_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?charset=utf8mb4"
-print("생성된 DB_URL:", DB_URL)
+# 데이터베이스 URL 가져오기
+DB_URL = os.getenv("DB_URL")
 
 # SQLAlchemy 엔진 생성
 engine = create_engine(DB_URL)
